c33bffafe2a44335b579e751c60b5bf8ef9f77f0
drtLaura <116120955+drtLaura@users.noreply.github.com>
2024-12-18 21:38:23 +0100

    Add files via upload

---
+++ admin-final/CRA.DataAccess/IScheduleRepository.cs
@@ -0,0 +1,21 @@
+﻿using CRA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRA.DataAccess
+{
+    public interface IScheduleRepository
+    {
+        IEnumerable<Schedule> GetAll();
+        Schedule GetById(Guid id);
+        IEnumerable<Schedule> GetByEmployeeId(Guid employeeId);
+        void Add(Schedule schedule);
+        void Update(Schedule schedule);
+        void Delete(Schedule schedule);
+
+    }
+}
---
+++ admin-final/CRA.DataAccess/ITimeSlotRepository.cs
@@ -0,0 +1,17 @@
+﻿using CRA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRA.DataAccess
+{
+    public interface ITimeSlotRepository
+    {
+        IEnumerable<SentTimeSlotViewModel> SentTimeSlots();
+        
+        TimeSlot GetTimeSlotById(Guid timeslot);
+        void UpdateTimeSlot(TimeSlot timeslot);
+    }
+}
